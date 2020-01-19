cd6e7f17fb599fa1ac2a1c3007c98eebd2b23d9d
kimkyunghun3 <kimkyunghun3@naver.com>
2020-01-19 21:29:37 +0900

    admin panel 보기쉽게 바꾸기

--- rooms/admin.py
+++ rooms/admin.py
@@ -13,6 +13,19 @@ class ItemAdmin(admin.ModelAdmin):
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
 
+    fieldsets = (
+        ("Basic Info", {"fields": ("name", "description", "country", "address", "price")},
+         ),
+        ("Times", {"fields": ("check_in", "check_out", "instant_book")}),
+        ("Spaces", {"fields": ("guests", "beds", "bedrooms", "baths")}),
+        ("More about the space", {
+            "classes": ("collapse",),
+         "fields": ("amenities", "facilities", "house_rules"),
+         },
+         ),
+        ("Last Details", {"fields": ("host",)}),
+    )
+
     list_display = (
         "name",
         "country",
@@ -28,9 +41,16 @@ class RoomAdmin(admin.ModelAdmin):
 
     )
 
-    list_filter = ("instant_book", "city", "country")
+    list_filter = ("instant_book", "host__superhost", "room_type",
+                   "amenities", "facilities", "house_rules", "city", "country")
 
     search_fields = ("^city", "^host__username")
+
+    filter_horizontal = (
+        "amenities",
+        "facilities",
+        "house_rules",
+    )
 
 
 @admin.register(models.Photo)
