352d561922381da60a8b7680d1a62641c36014e7
kimkyunghun3 <kimkyunghun3@naver.com>
2020-01-24 00:59:38 +0900

    admin패널에서 list_display할 수 없는 amenities 함수만들어서 적용

--- rooms/admin.py
+++ rooms/admin.py
@@ -38,8 +38,11 @@ class RoomAdmin(admin.ModelAdmin):
         "check_in",
         "check_out",
         "instant_book",
+        "count_amenities"
 
     )
+
+    ordering = ("name", "price", "bedrooms")
 
     list_filter = ("instant_book", "host__superhost", "room_type",
                    "amenities", "facilities", "house_rules", "city", "country")
@@ -51,6 +54,11 @@ class RoomAdmin(admin.ModelAdmin):
         "facilities",
         "house_rules",
     )
+
+    def count_amenities(self, obj):  # class 안의 함수에서 self은 class을 의미하고 obj은 admin에서 row을 의미
+        print(obj.amenities.all())
+        return "potato"
+    count_amenities.short_description = "hello sexy!"  # functionailty다. 장고에서 함수로 받아들인다
 
 
 @admin.register(models.Photo)
