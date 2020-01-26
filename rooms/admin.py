44e7718c60ce7af08320d3e6f73811714fecc2f0
kimkyunghun3 <kimkyunghun3@naver.com>
2020-01-26 12:43:27 +0900

    Inline을 이용하여 room 안에 Photo FK을 저장할 수 있도록 구성

--- rooms/admin.py
+++ rooms/admin.py
@@ -18,10 +18,17 @@ class ItemAdmin(admin.ModelAdmin):
     pass
 
 
+class PhotoInline(admin.TabularInline):
+
+    model = models.Photo
+
+
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
 
     """ Room Admin Definition """
+
+    inlines = (PhotoInline,)
 
     fieldsets = (
         (
